739aa356c06080cad65abd0bec1c070ceeceba45
RogerHagman <rogerhagman84@gmail.com>
2023-09-30 16:47:02 +0200

    0.06

--- main.py
+++ main.py
@@ -28,7 +28,14 @@ class ConfigManager(metaclass=SingletonMeta):
     def get_color_coordinates(self, color_name):
         return self.get_setting("color_coordinates").get(color_name)
 
-
+    def get_brightness(self, label="NEUTRAL"):
+        brightness_levels = self.get_setting("brightness_levels", {})
+        return brightness_levels.get(label, 150)
+    
+    def get_transition_time(self, label="SHORT"):
+        transition_times = self.get_setting("transition_times", {})
+        return transition_times.get(label, 0) # Default to None if not found.
+    
 class HueController:
     def __init__(self):
         self.config = ConfigManager()
@@ -43,19 +50,21 @@ class HueController:
         time.sleep(10)
         self.bridge.set_group(0, 'on', True)
 
-        
-        xy = self.config.get_color_coordinates("BLUE")
         settings = {
             # 'xy' color coordinates between 0 and 1
-            'xy': xy,
-            # 'bri' brightness between 0 and 255
-            'bri': self.config.get_setting("brightness", 254), 
-            # Transition time in milliseconds
-            'transitiontime': self.config.get_setting("transition_time", 50)
+            'xy': self.config.get_color_coordinates("GREEN"),
+            
+            'bri': # 'bri' brightness between 0 and 255
+            self.config.get_brightness("VERY_BRIGHT"),
+            
+            'transitiontime': # 'transitiontime' Transition time in milliseconds
+            self.config.get_transition_time("VERY_SHORT")
         }
         self.bridge.set_group(0, settings)
         time.sleep(10)
-        self.bridge.set_group(0, {'transitiontime': 50, 'on': False})
+        self.bridge.set_group(0, {
+            'transitiontime': self.config.get_transition_time("NONE"), 
+            'on': False})
 
 
 if __name__ == "__main__":
